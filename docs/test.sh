066fff4449eddc9f4d6253b4bcd5301797763b59
FlyApple <magicskyapple@gmail.com>
2023-08-12 19:51:27 +0800

    20230810-2

--- docs/test.sh
+++ docs/test.sh
@@ -2,8 +2,8 @@
 
 curl "https://open.dacn.me/api/v1/chat/completions" \
         -H 'Content-Type: application/json' \
-        -H 'Authorization: Bearer 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' \
-        -H 'OpenAI-Organization: org-92aU5J5z7KhYYRbksMIrgWKA' \
+        -H 'Authorization: Bearer sk-1234567890' \
+        -H 'OpenAI-Organization: org-1234567890' \
         -d '{
                 "model": "gpt-3.5-turbo",
                 "messages": [{"role": "user", "content": "Say this is a test!"}],
