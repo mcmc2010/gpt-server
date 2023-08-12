0eb378f64bdcf1bd2c93225e70a8f6899b1f7576
unknown <magicskyapple@gmail.com>
2023-08-13 02:11:49 +0800

    20230810-5

--- server/openaiapi.go
+++ server/openaiapi.go
@@ -3,6 +3,7 @@ package server
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 
@@ -46,48 +47,123 @@ type ChatGPTResponse struct {
 	} `json:"choices"`
 }
 
-func API_GPTModels(config Config) []*ChatGPTModel {
-	var models []*ChatGPTModel
-	url, _ := url.JoinPath(config.APIUrl, "/v1/models")
+type API_HTTPData struct {
+	url     string
+	Headers []string
+
+	//
+	Body   any
+	Length int
+
+	//
+	ErrorCode    int
+	ErrorMessage string
+}
+
+// HTTP Request
+func API_HTTPRequest(base_url string, path string, data *API_HTTPData) *API_HTTPData {
+	if data == nil {
+		data = &API_HTTPData{
+			url:     "",
+			Headers: nil,
+		}
+	}
+
+	url, _ := url.JoinPath(base_url, path)
+	data.url = url
+	data.Body = nil
+	data.Length = 0
+	data.ErrorCode = -1
+	data.ErrorMessage = "<null>"
 
 	//
 	var client = &http.Client{}
+	utils.Logger.Log("(API) Request URL: ", data.url)
+
+	//
 	request, err := http.NewRequest("Get", url, nil)
 	if err != nil {
 		utils.Logger.LogError("(API) Create Request Error: ", err)
 		return nil
 	}
-	request.Header.Set("Openai-Organization", config.APIOrganization)
-	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", config.APIKey))
+	request.Header.Set("Content-Type", "application/json;charset=utf-8")
+
+	count := len(data.Headers)
+	if count%2 > 0 {
+		utils.Logger.LogError("(API) Create Request Error: Add header error.")
+		return nil
+	}
+	for i := 0; i < count; i += 2 {
+		request.Header.Set(data.Headers[i+0], data.Headers[i+1])
+	}
+
+	//
 	response, err := client.Do(request)
 	if err != nil {
 		utils.Logger.LogError("(API) Request Error: ", err)
 		return nil
 	}
-	defer response.Body.Close()
 
-	if response.StatusCode != 200 {
-		utils.Logger.LogError("(API) Request ", "(Status:", response.StatusCode, ") Error: ", response.Status)
-		return nil
-	}
-
-	var bytes []byte
-	length, err := response.Body.Read(bytes)
+	bytes, err := io.ReadAll(response.Body)
 	if err != nil {
 		utils.Logger.LogError("(API) Response Body Error: ", err)
 		return nil
 	}
 
-	if length > 0 {
-		err = json.Unmarshal(bytes, &models)
-		if err != nil {
-			utils.Logger.LogError("(API) Response Body Error: ", err)
-			return nil
-		}
-	} else {
-		utils.Logger.LogError("(API) Response Body : (null)")
+	defer response.Body.Close()
+
+	//
+	if response.StatusCode != 200 {
+		body := string(bytes)
+
+		data.Body = body
+		data.Length = len(body)
+
+		utils.Logger.LogError("(API) Response Body Failed: ",
+			fmt.Sprintf("[(%d) Status:%s] ", response.StatusCode, response.Status))
+
+		data.ErrorCode = response.StatusCode
+		data.ErrorMessage = response.Status
 		return nil
 	}
+
+	var body any
+	err = json.Unmarshal(bytes, &body)
+	if err != nil {
+		utils.Logger.LogError("(API) Response Body JSON Format Error: ", err)
+
+		// Text
+		body := string(bytes)
+
+		data.Body = body
+		data.Length = len(body)
+
+		data.ErrorCode = -2
+		data.ErrorMessage = err.Error()
+		return nil
+	}
+
+	data.Body = body
+	data.Length = len(bytes)
+
+	data.ErrorCode = 0
+	data.ErrorMessage = ""
+	return data
+}
+
+// OpenAI API : Models
+func API_GPTModels(config Config) []*ChatGPTModel {
+
+	var models []*ChatGPTModel
+
+	data := API_HTTPData{
+		Headers: []string{
+			//"Openai-Organization", config.APIOrganization,
+			//"Authorization", fmt.Sprintf("Bearer %s", config.APIKey),
+		},
+	}
+	//API_HTTPRequest(config.APIUrl, "/v1/models", &data)
+	API_HTTPRequest("https://www.baidu.com", "", &data)
 
 	return models
 }
